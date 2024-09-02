2ad7dfca77e7f6c05371f17c7d8cb06f8f7e7b0b
Abdulloh <abdullajonmamanazirov@gmail.com>
2024-09-02 13:31:32 +0500

    add home page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/";
+import { Home } from "./pages";
+
 function App() {
-  return <>Hello World!</>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
