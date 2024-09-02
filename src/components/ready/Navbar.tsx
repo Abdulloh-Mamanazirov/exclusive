5ac79b99d5a6eab18e4a2e484d1150ce7fdc3177
Abdulloh <abdullajonmamanazirov@gmail.com>
2024-09-02 12:12:47 +0500

    add container class

--- src/components/ready/Navbar.tsx
+++ src/components/ready/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = (): React.ReactElement => {
 
   return (
     <div>
-      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:container">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link
             to="#"
