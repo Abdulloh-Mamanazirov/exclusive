6d0637420a1eceeecf54caaa8a015119ac4b3019
Abdulloh <abdullajonmamanazirov@gmail.com>
2024-09-12 08:31:24 +0500

    export about & contact pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,7 @@
 import Home from "./home";
 import Wishlist from "./wishlist";
 import Cart from "./cart";
+import About from "./about";
+import Contact from "./contact";
 
-export { Home, Wishlist, Cart };
+export { Home, Wishlist, Cart, About, Contact };
