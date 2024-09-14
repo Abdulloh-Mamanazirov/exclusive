51eef4a22e887a512b38b7f7738f8e8fed4da7a4
Abdulloh <abdullajonmamanazirov@gmail.com>
2024-09-14 08:38:50 +0500

    export productId page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,5 +3,6 @@ import Wishlist from "./wishlist";
 import Cart from "./cart";
 import About from "./about";
 import Contact from "./contact";
+import ProductId from "./productId";
 
-export { Home, Wishlist, Cart, About, Contact };
+export { Home, Wishlist, Cart, About, Contact, ProductId };
