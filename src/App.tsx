7b22d57a69375a521dcae74c43f746e133e4efae
Abdulloh <abdullajonmamanazirov@gmail.com>
2024-09-14 08:42:21 +0500

    scroll to the top when page changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Footer, Navbar } from "./components/";
 import { About, Cart, Contact, Home, ProductId, Wishlist } from "./pages";
 
 function App() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
